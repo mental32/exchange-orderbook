1f85e4b7ae931eac09932d45bb6a03c6518e1b5a
mental <mentalfoss@gmail.com>
2023-10-28 20:18:51 +0300

    feat: Add reserve functionality for fiat
    This commit adds the reserve functionality for fiat and crypto in the `AppCx` struct. It introduces the `ReserveFiat` struct to track the previous and new balance of reserved fiat funds. The `ReserveError` enum is also added to handle different types of reserve errors, such as insufficient funds or database errors.
    
    The `place_order` method now reserves fiat funds when placing a buy order and reserves crypto assets when placing a sell order. The `fiat_balance` method retrieves the current balance of fiat funds for a user, while the `reserve_fiat` method reserves a specified quantity of fiat funds from a user's account.
    
    Additionally, some changes are made to the database schema in the migration files. Triggers and functions related to accounting are dropped before dropping tables in the down.sql file, and an account for the exchange's cash account is inserted in the up.sql file.
    
    Note: The implementation for reserving crypto assets is still pending (`reserve_crypto` method).

--- migrations/2023-10-26-144215_create_accounting/down.sql
+++ migrations/2023-10-26-144215_create_accounting/down.sql
@@ -1,10 +1,10 @@
--- Drop the triggers and functions first
-DROP TRIGGER IF EXISTS update_timestamp ON accounts;
-DROP TRIGGER IF EXISTS validate_transaction ON tx_journal;
-DROP FUNCTION IF EXISTS update_timestamp();
-DROP FUNCTION IF EXISTS validate_transaction();
-
 -- Drop the tables
 DROP TABLE IF EXISTS account_tx_journal;
 DROP TABLE IF EXISTS accounts;
 DROP TABLE IF EXISTS account_sources;
+
+-- Drop the triggers and functions
+DROP TRIGGER IF EXISTS update_timestamp ON accounts;
+DROP TRIGGER IF EXISTS validate_transaction ON tx_journal;
+DROP FUNCTION IF EXISTS update_timestamp();
+DROP FUNCTION IF EXISTS validate_transaction();
