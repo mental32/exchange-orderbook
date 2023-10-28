fee378b0fdbc4e89bbe9a323220a93c2b2d17b93
mental <mentalfoss@gmail.com>
2023-10-29 00:09:53 +0300

    feat: Update PlaceOrder function signature and error handling
    - Updated the return type of the `do_place_order` function to return a `PlaceOrderResult` instead of an `OrderUuid`.
    - Updated the type alias `PlaceOrderTx` to send a result of `PlaceOrderResult` instead of `OrderUuid`.
    - Added a new struct `PlaceOrderResult` to hold information about the original order and its result.
    - Added a new enum `PlaceOrderError` to handle errors related to placing an order.
    - Modified the code in various places to use the updated types and handle errors appropriately.

--- exchange/src/trading/timeinforce.rs
+++ exchange/src/trading/timeinforce.rs
@@ -1,13 +1,17 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub enum TimeInForce {
+    /// Good Til Canceled, default. The order will remain open until it is either filled or canceled.
     #[serde(rename = "gtc")]
     GoodTilCanceled,
+    /// Good Til Date specified. The order will remain open until it is either filled or canceled. it will automatically cancel at the specified timestamp.
     #[serde(rename = "gtd")]
     GoodTilDate,
+    /// Immediate Or Cancel. The order must be filled immediately and any unfilled portion of the order will be canceled.
     #[serde(rename = "ioc")]
     ImmediateOrCancel,
+    /// Fill Or Kill. The order must be filled immediately in its entirety or it will be canceled. The difference between this and IOC is that GTC orders will be placed on the book until canceled instead of being canceled at the end of the trading day.
     #[serde(rename = "fok")]
     FillOrKill,
 }
