ee13f803c920d0514a2a2c9c5a33e0745c9a3191
mental <mentalfoss@gmail.com>
2023-11-21 04:48:50 +0200

    feat: add sign-in and sign-up, use tailwind to not look ugly.

---
+++ frontend/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Navbar from './Navbar.svelte';
+	import './app.css';
+</script>
+
+<div id="app">
+	<Navbar />
+	<!-- Rest of your content -->
+</div>
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,7 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 export default {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	preprocess: vitePreprocess(),
 };
