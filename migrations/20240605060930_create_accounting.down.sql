87f1eb9b337c9ee7e0e8b3c0367d9ec3d98fc77d
mental <mentalfoss@gmail.com>
2024-07-09 09:09:38 +0100

    Correct 'down' SQL for migrations

--- migrations/20240605060930_create_accounting.down.sql
+++ migrations/20240605060930_create_accounting.down.sql
@@ -1,2 +1,19 @@
-DROP TABLE IF EXISTS accounts;
+-- Down migration for accounts and account_tx_journal tables, triggers, and functions
+
+-- Drop the validate_transaction trigger
+DROP TRIGGER IF EXISTS validate_transaction ON account_tx_journal;
+
+-- Drop the validate_transaction function
+DROP FUNCTION IF EXISTS validate_transaction;
+
+-- Drop the update_timestamp trigger
+DROP TRIGGER IF EXISTS update_timestamp ON accounts;
+
+-- Drop the update_timestamp function
+DROP FUNCTION IF EXISTS update_timestamp;
+
+-- Drop the account_tx_journal table
 DROP TABLE IF EXISTS account_tx_journal;
+
+-- Drop the accounts table
+DROP TABLE IF EXISTS accounts;
