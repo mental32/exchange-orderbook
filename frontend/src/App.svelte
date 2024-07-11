839f58739d2c108241a7b9dd2c4efd0ff49148ac
mental <mentalfoss@gmail.com>
2024-07-11 18:34:25 +0100

    Serve frontend statically

--- frontend/src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Navbar from './Navbar.svelte';
-	import './app.css';
-</script>
-
-<div id="app" class="bg-[#EEECFB]">
-	<Navbar />
-	<!-- Rest of your content -->
-</div>
--- frontend/svelte.config.js
+++
@@ -1,9 +0,0 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-
-export default {
-	kit: {
-		adapter: adapter()
-	},
-	preprocess: vitePreprocess(),
-};
