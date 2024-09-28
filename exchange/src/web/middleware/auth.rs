38ce7dcbf50939980f5d3d339385940e614cca14
mental <mentalfoss@gmail.com>
2024-09-28 14:09:38 +0100

    feat: Add new HTML index route and middleware functions
    Add a new HTML index route for rendering front-page.html.jinja.
    Implement necessary error handling and response conversion.
    Include new functions for session validation and redirection in auth middleware.

--- exchange/src/web/middleware/auth.rs
+++ exchange/src/web/middleware/auth.rs
@@ -2,12 +2,12 @@ use std::str::FromStr;
 
 use axum::body::Body;
 use axum::extract::State;
-use axum::http::{Request, StatusCode};
+use axum::http::{HeaderMap, Request, StatusCode};
 use axum::middleware::Next;
 use axum_extra::headers::authorization::Bearer;
 use axum_extra::headers::{Authorization, HeaderMapExt};
 
-use axum::response::IntoResponse;
+use axum::response::{IntoResponse, Redirect};
 use axum_extra::extract::CookieJar;
 use chrono::{Datelike, TimeZone, Timelike};
 use sqlx::types::time::{Date, PrimitiveDateTime, Time};
@@ -28,20 +28,46 @@ pub struct UserUuid(pub uuid::Uuid);
 ///
 pub async fn validate_session_token(
     State(state): State<InternalApiState>,
-    // jar: CookieJar,
     mut request: Request<Body>,
     next: Next,
 ) -> axum::response::Response {
+    match try_validate_session(state, request.headers()).await {
+        Ok(user_uuid) => {
+            request.extensions_mut().insert(user_uuid);
+            next.run(request).await
+        }
+        Err(err) => err.into_response(),
+    }
+}
+
+pub async fn validate_session_token_or_redirect(
+    State(state): State<InternalApiState>,
+    mut request: Request<Body>,
+    next: Next,
+) -> axum::response::Response {
+    match try_validate_session(state, request.headers()).await {
+        Ok(user_uuid) => {
+            request.extensions_mut().insert(user_uuid);
+            next.run(request).await
+        }
+        Err((StatusCode::UNAUTHORIZED, _)) => Redirect::to("/").into_response(),
+        Err(err) => err.into_response(),
+    }
+}
+
+pub async fn try_validate_session(
+    state: InternalApiState,
+    headers: &HeaderMap,
+) -> Result<UserUuid, (StatusCode, &'static str)> {
     // Extract the session-token cookie from the request headers
-    let jar = CookieJar::from_headers(request.headers());
+    let jar = CookieJar::from_headers(headers);
     let session_token = if let Some(t) = jar.get("session-token") {
         t.value_trimmed()
     } else {
-        return (
+        return Err((
             StatusCode::UNAUTHORIZED,
             "Unauthorized: session-token cookie missing",
-        )
-            .into_response();
+        ));
     };
 
     let rec = match sqlx::query!(
@@ -54,7 +80,7 @@ pub async fn validate_session_token(
         Ok(r) => r,
         Err(err) => {
             tracing::error!(?err, "session-token select failure");
-            return (StatusCode::INTERNAL_SERVER_ERROR, "Try again later").into_response();
+            return Err((StatusCode::INTERNAL_SERVER_ERROR, "Try again later"));
         }
     };
 
@@ -68,24 +94,21 @@ pub async fn validate_session_token(
             .unwrap();
 
             if now >= expires {
-                return (
+                return Err((
                     StatusCode::UNAUTHORIZED,
                     "Unauthorized: session-token has expired",
-                )
-                    .into_response();
+                ));
             }
 
             // Session token is valid; proceed to the next middleware or handler
-            request.extensions_mut().insert(UserUuid(rec.user_id));
-            next.run(request).await
+            Ok(UserUuid(rec.user_id))
         }
         None => {
             // Session token is invalid; return an unauthorized error
-            (
+            Err((
                 StatusCode::UNAUTHORIZED,
                 "Unauthorized: invalid session token",
-            )
-                .into_response()
+            ))
         }
     }
 }
--- exchange/src/web/session_create.rs
+++ exchange/src/web/session_create.rs
@@ -3,122 +3,82 @@ use std::str::FromStr;
 
 use argon2::password_hash::PasswordHashString;
 use argon2::{Argon2, PasswordVerifier};
-use axum::extract::State;
-use axum::http::header::{CONTENT_TYPE, SET_COOKIE};
-use axum::http::{HeaderMap, HeaderValue, StatusCode};
+use axum::extract::{ConnectInfo, State};
+use axum::http::header::{CONTENT_TYPE, SET_COOKIE, USER_AGENT};
+use axum::http::{HeaderMap, HeaderName, HeaderValue, StatusCode};
 use axum::response::{AppendHeaders, IntoResponse, IntoResponseParts, Response};
-use axum::Json;
+use axum::{Form, Json};
 use axum_extra::extract::cookie::{self, Cookie};
 use axum_extra::extract::CookieJar;
+use axum_htmx::HxRequest;
 use email_address::EmailAddress;
 use serde::{Deserialize, Serialize};
 use sqlx::types::time::PrimitiveDateTime;
 
+use crate::app_cx::VerifyLoginDetailsError;
+use crate::password::{de_password_from_str, Password};
+use crate::web::middleware::ip_address::rightmost_ip_address;
+
 use super::InternalApiState;
 
-#[derive(Clone, Serialize, Deserialize)]
-struct Password(String);
-
-impl std::fmt::Debug for Password {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("Password").field(&"*").finish()
-    }
-}
-
-impl Deref for Password {
-    type Target = str;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct SessionCreate {
     email: EmailAddress,
+    #[serde(deserialize_with = "de_password_from_str")]
     password: Password,
 }
 
 pub async fn f(
     State(state): State<InternalApiState>,
     jar: CookieJar,
-    Json(body): Json<SessionCreate>,
+    headers: HeaderMap,
+    ConnectInfo(connect_info): ConnectInfo<std::net::SocketAddr>,
+    HxRequest(hx): HxRequest,
+    Form(body): Form<SessionCreate>,
 ) -> Response {
+    use VerifyLoginDetailsError as V;
     tracing::trace!(?body, "session_create");
 
-    let db = state.db();
+    let ip_address = rightmost_ip_address(&headers).unwrap_or(connect_info.ip());
+    let user_agent = headers
+        .get(USER_AGENT)
+        .and_then(|hv| hv.to_str().ok())
+        .map(|st| st.to_owned());
 
-    let rec = match sqlx::query!(
-        // language=PostgreSQL
-        r#"
-        SELECT id, password_hash FROM users
-        WHERE email = $1
-        "#,
-        body.email.as_str()
-    )
-    .fetch_one(&db)
-    .await
+    let user_uuid = match state
+        .verify_login_details(&body.email, &body.password)
+        .await
     {
-        Ok(rec) => rec,
-        Err(sqlx::Error::RowNotFound) => {
-            tracing::info!(email = ?body.email, "user email not found");
-            return StatusCode::UNAUTHORIZED.into_response();
-        }
-        Err(err) => {
-            tracing::error!(?err, "failed to query user");
+        Ok(user_uuid) => user_uuid,
+        Err(V::Unauthorized) => return StatusCode::UNAUTHORIZED.into_response(),
+        Err(V::Other(_)) => {
             return StatusCode::INTERNAL_SERVER_ERROR.into_response();
         }
     };
 
-    tracing::info!(user_id = ?rec.id, "user found");
-
-    if tokio::task::spawn_blocking({
-        let from_utf8 = &String::from_utf8(rec.password_hash).unwrap();
-        let phs = PasswordHashString::from_str(from_utf8.as_str()).unwrap();
-        let password_as_bytes = body.password.as_bytes().to_owned();
-
-        move || {
-            Argon2::default()
-                .verify_password(&password_as_bytes, &phs.password_hash())
-                .is_err()
+    let session_token = match state
+        .create_session(user_uuid, Some(ip_address), user_agent)
+        .await
+    {
+        Ok(st) => st,
+        Err(err) => {
+            tracing::error!(?err, "could not create session");
+            return StatusCode::INTERNAL_SERVER_ERROR.into_response();
         }
-    })
-    .await
-    .unwrap_or(false)
-    {
-        tracing::info!("password mismatch");
-        return StatusCode::UNAUTHORIZED.into_response();
-    }
-
-    // generate a session token and store it
-    let session_token = {
-        let mut rng = rand::thread_rng();
-        let mut bytes = [0u8; 32];
-        rand::Rng::fill(&mut rng, &mut bytes[..]);
-        hex::encode(bytes)
-    };
-
-    if let Err(err) = sqlx::query!(
-        "INSERT INTO session_tokens (token, max_age, user_id) VALUES ($1, $2, $3);",
-        session_token.as_bytes(),
-        3600,
-        rec.id,
-    )
-    .execute(&state.db())
-    .await
-    {
-        tracing::error!(?err, "session_token insert failure");
-        return StatusCode::INTERNAL_SERVER_ERROR.into_response();
     };
 
     tracing::info!(?session_token, "session created");
 
     let session_token_cookie = Cookie::build(("session-token", session_token.as_str()))
         .max_age(time::Duration::hours(1))
+        .path("/")
         .to_string();
 
     (
-        AppendHeaders([(SET_COOKIE, session_token_cookie)]),
+        AppendHeaders([
+            (SET_COOKIE, session_token_cookie),
+            (HeaderName::from_static("hx-redirect"), "/c".to_string()),
+        ]),
         StatusCode::CREATED,
     )
         .into_response()
